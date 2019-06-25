e9ccf12b0828f70360851568808fde346c604e3b
TromboneSolo <micahsorkin@protonmail.com>
2019-06-24 19:52:23 -0700

    css changes

--- src/RidewatchList.jsx
+++ src/RidewatchList.jsx
@@ -13,23 +13,49 @@ export class RidewatchList extends Component {
     this.onClick = this.onClick.bind(this);
   }
 
+  componentDidMount() {
+    this.setState({});
+  }
+
   onClick() {
-    this.setState({
-      Collapsed: true
-    })
+    if (this.state.Collapsed === false) {
+      this.setState({
+        Collapsed: true,
+        series: this.props.series + "-closed",
+        imgsrc: [".png"]
+      });
+    } else {
+      this.setState({
+        Collapsed: false,
+        series: this.props.series,
+        imgsrc: this.props.imgsrc
+      });
+    }
   }
 
   render() {
-    return (
-      <div className={this.props.series}>
-        <h1 onClick={this.onClick}>{this.state.series}</h1>
-        <ul>
-          {this.state.imgsrc.map(i => {
-            return <li>{<Ridewatch imgsrc={i} alt={i.toString()} />}</li>;
-          })}
-        </ul>
-      </div>
-    );
+    if (this.state.Collapsed === true) {
+      return (
+        <div className={this.state.series + "-div"}>
+          <h1 onClick={this.onClick} className={this.state.series}>
+            {this.props.series}
+          </h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className={this.state.series + "-div"}>
+          <h1 onClick={this.onClick} className={this.state.series}>
+            {this.state.series}
+          </h1>
+          <ul className = {this.state.series + "-list"}>
+            {this.state.imgsrc.map(i => {
+              return <li>{<Ridewatch imgsrc={i} alt={i.toString()} />}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
